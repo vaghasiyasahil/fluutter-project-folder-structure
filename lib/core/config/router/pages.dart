753d82c45e0b20f76fcaab1578dba32368b0df5a
Sahil Vaghasiya <129865823+vaghasiyasahil@users.noreply.github.com>
2025-09-20 18:37:41 +0530

    internet screen page add

--- lib/core/config/router/pages.dart
+++ lib/core/config/router/pages.dart
@@ -3,5 +3,6 @@ import '../../../export.dart';
 class Pages {
   List<GetPage<dynamic>> pages = [
     GetPage(name: Routes.splashScreen, page: () => SplashScreen()),
+    GetPage(name: Routes.internetScreen, page: () => InternetScreen()),
   ];
 }
